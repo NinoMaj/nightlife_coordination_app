5bed9326bda27eed1e6a8fe1bde575293747c877
NinoMaj <nino.majder@gmail.com>
2017-04-13 00:01:31 +0200

    dotenv change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-require('dotenv').load();
+require('dotenv').config()
 // connect to the database and load models
 require('./server/models').connect(process.env.DB_URI_HEROKU);
 
